9803a36acb29974ab17fa8c2398a0586e5c76ba3
Atuuh <not-another-one@hotmail.com>
2020-07-05 17:31:16 +0100

    More attempts

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,23 +1,30 @@
+require("dotenv").config();
+
 import WebHookListener from "twitch-webhooks";
 import twitch from "twitch";
 
 const twitchClientId = process.env.TWITCH_CLIENT_ID || "";
 const twitchClientSecret = process.env.TWITCH_CLIENT_SECRET || "";
 
+console.log("PORT", process.env.PORT);
+
 const setup = async () => {
+    const port = Number(process.env.PORT);
+
     const twitchClient = await twitch.withClientCredentials(
         twitchClientId,
         twitchClientSecret
     );
 
     const listener = await WebHookListener.create(twitchClient, {
-        hostName: "68884b3dc79a.ngrok.io",
-        port: Number(process.env.PORT) || 8090,
-        reverseProxy: { port: 80, ssl: true },
+        port: 8090,
+        reverseProxy: { port: port, ssl: true },
         logger: { minLevel: "trace" },
     });
+    console.log("PORT", process.env.PORT);
 
     listener.listen();
+    console.log("PORT", process.env.PORT);
 
     const atuuh = await twitchClient.helix.users.getUserByName("atuuh");
     console.log("atuuh", atuuh);
