355a7317f9d23c36eaf57cf80faec0c22468ab4c
Atuuh <not-another-one@hotmail.com>
2020-08-16 12:03:02 +0100

    Removed extra registration of module

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import "reflect-metadata";
 (async () => {
     const app = await App.create();
 
-    await app.registerModule(TwitchAlertModule);
-
     process.on("SIGINT", app.shutdown);
     process.on("SIGTERM", app.shutdown);
 })();
